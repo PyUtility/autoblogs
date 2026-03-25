cdafe7c37e77d6a8aa5520e8f480146881be30a7
ZenithClown <debmalyapramanik.005@gmail.com>
2026-03-25 23:23:47 +0530

    ⚙ render cli layout, update resopnes, requirements file

--- autoblogs/cli/main.py
+++ autoblogs/cli/main.py
@@ -4,17 +4,59 @@
 CLI Application for AutoBlogs - Content Creation Tool
 """
 
+import os
 import getpass
+import pathlib
+
+from dotenv import load_dotenv
 
 from autoblogs.prompts import PROMPT_BLOGS
+from autoblogs.client.claude import ClaudeClient
 from autoblogs.client.openai import OpenAIClient
+from autoblogs.config.constants import AIProvider
 from autoblogs.model.dataflows import AIModel, AIRequest, AIResponse
 
-topic = input("Topic: ")
-prompt = input("One Line Prompt: ")
-apikey = getpass.getpass("API Key: ")
+def config(username : str, homepage : str) -> None:
+    root = pathlib.Path(__file__).parent / "static"
+    art = open(root / "ascii.graffiti.txt", "r").read()
+
+    print(f"\033[96m{art}\033[0m", end = "\n\n")
+    print(
+        f"Welcome \033[1m{username}\033[0m to AutoBlogs CLI! "
+        "Please enter the following information (or select defaults)."
+        f"\n\033[1m\033[92m  >> Homepage: {homepage}\033[0m\n\n"
+    )
+
+    global topic, prompt, aiclient, apikey, baseuri
+
+    providers = " | ".join([
+        provider.name for provider in AIProvider # type: ignore
+    ])
+    print(f"Available AI Provider  : {providers}")
+    provider =input("Select the AI Provider : ")
+
+    aiclient = {
+        "CLAUDE" : ClaudeClient
+    }.get(provider, OpenAIClient)
+
+    print("\n\nSet the API Key in Environment Variable\n")
+
+    topic  = input("Topic of the Blog      : ")
+    prompt = input("Elaborate Requirements : ")
+
+    load_dotenv()
+    apikey = os.getenv("LLM_MODEL_APIKEY")
+    baseuri = os.getenv("LLM_API_BASE_URL") \
+        if provider == "NVIDIA-NIM" else None
+    return
+
 
 def launch() -> AIResponse:
+    config(
+        username = getpass.getuser(),
+        homepage = "https://github.com/PyUtility/autoblogs"
+    )
+
     model = AIModel(
         model = "nvidia/nemotron-content-safety-reasoning-4b"
     )
@@ -22,9 +64,8 @@ def launch() -> AIResponse:
         topic = topic, prompt = prompt, context = PROMPT_BLOGS
     )
 
-    client = OpenAIClient(
-        model = model, apikey = apikey,
-        base_url = "https://integrate.api.nvidia.com/v1"
+    client = aiclient(
+        model = model, apikey = apikey, base_url = baseuri # type: ignore
     )
 
     response = client.generate(request = request)
