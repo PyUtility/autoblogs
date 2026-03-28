d18c926d9665f2def38604b741b0ba30f2ef57b7
ZenithClown <debmalyapramanik.005@gmail.com>
2026-03-29 00:16:35 +0530

    🩹 updated .env.example and removed deprecated defaults

--- autoblogs/config/default.py
+++ autoblogs/config/default.py
@@ -4,11 +4,4 @@
 Define the Module Level Default Value(s) for AutoBlogs
 """
 
-from autoblogs.config.constants import AIProvider
-
 homepage = "https://github.com/PyUtility/autoblogs"
-newissue = f"{homepage}/issues/new/choose"
-
-defaultmodel = AIProvider.LOCAL
-
-__all__ = ["homepage", "newissue", "defaultmodel"]
